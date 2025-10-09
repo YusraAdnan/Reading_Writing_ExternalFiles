b666648c2da52983e6b2ac4f6ffc68bdd58a2c6d
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-10-09 23:53:42 +0200

    Question 1 20222

---
+++ src/main/java/com/mycompany/fuel/Fuel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fuel;
+
+/**
+ *
+ * @author yusra
+ */
+public class Fuel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
